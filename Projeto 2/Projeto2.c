3db3cb1456f932c9ae63a95d50f9ebb644e86028
Guilherme Schmidt <101731542+guilherme-ls@users.noreply.github.com>
2024-11-05 13:02:03 -0300

    Atualiza arquivos

--- Projeto 2/Projeto2.c
+++ Projeto 2/Projeto2.c
@@ -1,28 +1,6 @@
-int ucContador = -1; // variÃ¡vel global para contador do display
+int ucContador = -1; // variável global para contador do display
 
-void INTERRUPCAO() iv 0x0008 ics ICS_AUTO {
-	if(INTCON.TMR0IF == 1) { // verifica se timer finalizou contagem
-		INTCON.TMR0IF = 0; // zera o flag de overflow da contagem
-
-		// valores iniciais carregados no timer para contagem atÃ© 1s na razÃ£o 32 e modo 16 bits
-		TMR0L = 0xDC;
-		TMR0H = 0x0B;
-
-		// incrementa valor no display de 7 segmentos
-		Incrementa();
-	}
-	if(INTCON.INT0IF == 1) { // verifica se a interrupÃ§Ã£o INT0 ocorreu
-		T0CON = 0B10000100; // liga o timer e configura prescaler para 32, contando 1s 
-		INTCON.INT0IF = 0; // zera flag de interrupÃ§Ã£o
-	}
-
-	if(INTCON3.INT1IF == 1) { // verifica se a interrupÃ§Ã£o INT1 ocorreu
-		T0CON = 0B10000010;; // liga o timer e configura prescaler para 8, contando 0.25s
-		INTCON3.INT1IF = 0; // zera flag de interrupÃ§Ã£o
-	}
-}
-
-void Incrementa() { // funÃ§Ã£o para incrementar contador e atualizar display
+void Incrementa() { // função para incrementar contador e atualizar display
 	ucContador++; // incrementa contador
 	if(ucContador > 9) // retorna contador a 0 caso este ultrapasse 9
 		ucContador = 0;
@@ -38,17 +16,38 @@ void Incrementa() { // funÃ§Ã£o para incrementar contador e atualizar display
 		case 7:{ latd = 0b00000111; break;}
 		case 8:{ latd = 0b01111111; break;}
 		case 9:{ latd = 0b01101111; break;}
-		default:{ PORTD =0; ucContador = -1; break;} // zera todo o PORTD em caso de entrada invÃ¡lida
+		default:{ PORTD =0; ucContador = -1; break;} // zera todo o PORTD em caso de entrada inválida
 	}
 }
 
+void INTERRUPCAO() iv 0x0008 ics ICS_AUTO {
+	if(INTCON.TMR0IF == 1) { // verifica se timer finalizou contagem
+		INTCON.TMR0IF = 0; // zera o flag de overflow da contagem
+
+		// valores iniciais carregados no timer para contagem até 1s na razão 32 e modo 16 bits
+		TMR0L = 0xDC;
+		TMR0H = 0x0B;
+
+		// incrementa valor no display de 7 segmentos
+		Incrementa();
+	}
+	if(INTCON.INT0IF == 1) { // verifica se a interrupção INT0 ocorreu
+		T0CON = 0B10000100; // liga o timer e configura prescaler para 32, contando 1s
+		INTCON.INT0IF = 0; // zera flag de interrupção
+	}
+
+	if(INTCON3.INT1IF == 1) { // verifica se a interrupção INT1 ocorreu
+		T0CON = 0B10000010;; // liga o timer e configura prescaler para 8, contando 0.25s
+		INTCON3.INT1IF = 0; // zera flag de interrupção
+	}
+}
 
 void ConfigMCU() {
 	// configura pinos do microcontrolador como digitais
 	ADCON1 |= 0X0F;
 
 	// habilita resistores de pull-up internos do PORTB
-	INTCON2.RBPU = 0; // RBPU Ã© barrado, portanto acionado em 0
+	INTCON2.RBPU = 0; // RBPU é barrado, portanto acionado em 0
 
 	TRISD = 0;  // definir porta D como saida
 	PORTD = 0;  // LEDs inicialmente apagados
@@ -59,23 +58,23 @@ void ConfigMCU() {
 }
 
 void ConfigInterrupt() {
-	// configuraÃ§Ã£o global das interrupÃ§Ãµes GIE
-	INTCON.GIEH = 1; // habilita interrupÃ§Ãµes
+	// configuração global das interrupções GIE
+	INTCON.GIEH = 1; // habilita interrupções
 
 	RCON.IPEN = 1; // habilita niveis de prioridade
 
-	INTCON.INT0IF = 0; // flag responsÃ¡vel por gerar/acionar a interrupÃ§Ã£o INT0
-	INTCON.INT0IE = 1; // habilita a interrupÃ§Ã£o especÃ­fica INT0
+	INTCON.INT0IF = 0; // flag responsável por gerar/acionar a interrupção INT0
+	INTCON.INT0IE = 1; // habilita a interrupção específica INT0
 
-	INTCON3.INT1IF = 0; // flag responsÃ¡vel por gerar/acionar a interrupÃ§Ã£o INT1
+	INTCON3.INT1IF = 0; // flag responsável por gerar/acionar a interrupção INT1
 	INTCON3.INT1IP = 1; // coloca INT1 como alta prioridade
-	INTCON3.INT1IE = 1; // habilita a interrupÃ§Ã£o especÃ­fica INT1
+	INTCON3.INT1IE = 1; // habilita a interrupção específica INT1
 
-	// ativa interrupÃ§Ãµes ao soltar o botÃ£o (rising edge)
+	// ativa interrupções ao soltar o botão (rising edge)
 	INTCON2.INTEDG0 = 1;
 	INTCON2.INTEDG1 = 1;
 
-	// ativa interrupÃ§Ã£o do timer 0
+	// ativa interrupção do timer 0
 	INTCON.TMR0IE = 1;
 }
 
@@ -86,13 +85,13 @@ void main() {
 	// configura timer, inicialmente desligado
 	T0CON = 0B00000100;
 
-	// valores iniciais carregados no timer para contagem atÃ© 1s na razÃ£o 32 e modo 16 bits
+	// valores iniciais carregados no timer para contagem até 1s na razão 32 e modo 16 bits
 	TMR0L = 0xDC;
 	TMR0H = 0x0B;
 
 	// flag de overflow do timer inicialmente zerada
 	INTCON.TMR0IF = 0 ;
 
-	// prende execuÃ§Ã£o neste loop
+	// prende execução neste loop
 	while(1);
 }
